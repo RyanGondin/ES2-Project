4ff697a8d45688a52aa9f431832efd5de844a96c
MrTimnn <pv23009@alunos.estgv.ipv.pt>
2025-03-24 14:21:33 +0000

    correção sinlgeton configuration

--- src/AppInstance.java
+++ src/AppInstance.java
@@ -1,17 +1,18 @@
 public class AppInstance {
     // Eager initialization
     private static final AppInstance instance = new AppInstance();
-    private ConfigurationManager configurationManager = new ConfigurationManager();
 
-    // Private constructor to prevent instantiation
-    private AppInstance() {}
+    // Private constructor to prevent instantiation from outside
+    private AppInstance() {
+    }
 
     // Public method to provide access to the instance
     public static AppInstance getInstance() {
         return instance;
     }
 
+    // Use ConfigurationFactory to get the ConfigurationManager instance
     public ConfigurationManager getConfigurationManager() {
-        return configurationManager;
+        return ConfigurationFactory.getConfigurationManager();
     }
 }
